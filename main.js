2ec02a0a937b8485b14c83f3bea95f88ca71f9c6
CleitonB3 <ville3003@gmail.com>
2024-05-02 19:56:16 -0300

    update commit

--- main.js
+++ main.js
@@ -7,11 +7,10 @@ form.addEventListener('submit', function (e) {
 function comparar() {
     let tn1 = document.getElementById('txtn1').value;
     let tn2 = document.getElementById('txtn2').value;
-    console.log(tn1, tn2)
-    if (parseInt(tn1) > parseInt(tn2)){
-        window.alert(`O número: ${tn1} é maior que ${tn2}`);
+    if (parseInt(tn1) < parseInt(tn2)){
+        window.alert(`O número: ${tn2} é maior que ${tn1}`);
     } else {
-        window.alert(`O número: ${tn1} é menor que ${tn2}`);
+        window.alert(`O número: ${tn2} é menor que ${tn1}`);
     }
 }
 
